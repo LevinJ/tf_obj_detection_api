dcd59262eaaa6556a8a00e04ff08221ef0525818
Levin Jian <jianzhirong@gmail.com>
2020-10-29 13:24:35 +0800

    create the split

--- create_splits.py
+++ create_splits.py
@@ -4,6 +4,7 @@ import os
 import random
 
 import numpy as np
+import shutil
 
 from utils import get_module_logger
 
@@ -17,6 +18,34 @@ def split(data_dir):
         - data_dir [str]: data directory, /mnt/data
     """
     # TODO: Implement function
+    training_files = glob.glob(data_dir + '/*.tfrecord')
+    training_files.sort()
+    num = len(training_files)
+    
+    # create the directry
+    for _dir in ["train", "val", "test"]:
+        dir_path = "{}/{}".format(data_dir, _dir)
+        dir_path = os.path.abspath(dir_path)
+        os.makedirs(dir_path, exist_ok=True)
+    
+    #split out the training part, 0.8
+    start = 0
+    end = start + int(0.8 * num)
+    for file_path in training_files[start:end]:
+        dst_path = "{}/train/{}".format(data_dir, os.path.basename(file_path))
+        shutil.move(file_path, dst_path)
+    #split out the val part, 0.1
+    start = end
+    end = start + int(0.1 * num)
+    for file_path in training_files[start:end]:
+        dst_path = "{}/val/{}".format(data_dir, os.path.basename(file_path))
+        shutil.move(file_path, dst_path)
+    
+    #split out the val part, 0.1
+    for file_path in training_files[end:]:
+        dst_path = "{}/test/{}".format(data_dir, os.path.basename(file_path))
+        shutil.move(file_path, dst_path)
+    return
     
 
 if __name__ == "__main__": 
